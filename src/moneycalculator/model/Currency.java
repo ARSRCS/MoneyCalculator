e603e11593ea859f98ae1985cf441b5c42679595
DanielRiveroG <danielriveroguerra@hotmail.com>
2016-12-26 10:29:14 +0000

    Con modelo

---
+++ src/moneycalculator/model/Currency.java
@@ -0,0 +1,26 @@
+package moneycalculator.model;
+
+public class Currency {
+    private final String symbol;
+    private final String code;
+    private final String name;
+
+    public Currency(String symbol, String code, String name) {
+        this.symbol = symbol;
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+}
